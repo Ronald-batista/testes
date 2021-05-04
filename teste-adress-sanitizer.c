60e7c7a6d89acf4b302f50271dab48f9e73513b0
Ronald-batista <ronald_hobo@outlook.com.br>
2021-05-04 14:03:46 -0300

    teste adress-sanitizer

---
+++ teste-adress-sanitizer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main(int argc, const char *argv[])
+{
+    char msg[6];
+ 
+    strcpy(msg, "Hello!");
+ 
+    printf("%s\n", msg);
+ 
+    return 0;
+}
